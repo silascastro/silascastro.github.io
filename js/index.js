0508521821298748f980d59d91d442dcf0c91cb5
silascastro <silascastro15@gmail.com>
2021-03-05 00:23:14 -0400

    ativando função de enviar email

--- js/index.js
+++ js/index.js
@@ -2,10 +2,39 @@ const submitButton = document.getElementById('submitEmail');
 
 
 submitButton.addEventListener('click', (e)=>{
-    $.ajax({
+    /*$.ajax({
         url: "https://api.github.com",
           success: function( result ) {
             console.log(result);
           }
+    });*/
+    var email = $("#email").val();
+    var message = $("#message").val();
+    
+  
+  if(email === "" || message === ""){
+    if(email === "" && message === ""){
+      alert("Os campos Email e Mensagem estão vazios!")
+    }
+
+    else if(email === ""){
+      alert("O campo Email não pode ser vazio");
+    }
+
+    else if(message === ""){
+      alert("O campo Mensagem não pode ser vazio");
+    }
+  }
+  else{
+    $.ajax({
+      type: "GET",
+      url: `https://crenulate-pounds.000webhostapp.com/?email=`+email+`&message=`+message,
+      sucess:function(result){
+        console.log(result);
+      }
     });
-});
+
+    location.reload();
+    alert("mensagem enviada com sucesso!")
+  }
+}) ;
